b76f28e4797033ddf3a54392002fcc822d790021
Nafis Faysal <mirzanafisfaysal@gmail.com>
2019-12-09 01:32:20 +0600

    add codebase and docker composer

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func main() {
+	data, err := ioutil.ReadFile("images.json")
+	if err != nil {
+		fmt.Errorf("can't read json data %s", err)
+		return
+	}
+
+	var result []string
+	err = json.Unmarshal(data, &result)
+	if err != nil {
+		fmt.Errorf("error on unmarshal json %s", err)
+		return
+	}
+
+	for i, url := range result {
+		i++
+		fmt.Printf("#%d - Downloading %s\n", i, url)
+		DownloadImages(url, strconv.Itoa(i)+"golang.jpg")
+		fmt.Printf("#%d - Completed %s\n", i, url)
+	}
+}
+
+func DownloadImages(url string, filepath string) error {
+	CreateDirIfNotExist("data")
+	// Create the file
+	out, err := os.Create("data/" + filepath)
+	checkError(err)
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Check server response
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %s", resp.Status)
+	}
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func CreateDirIfNotExist(dir string) {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err = os.MkdirAll(dir, 0755)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
